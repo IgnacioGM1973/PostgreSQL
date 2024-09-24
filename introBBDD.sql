948ae090d6e7e536bcf1f790037f38f70eb590c2
IgnacioGM1973 <I.GUIJARRO@HOTMAIL.ES>
2024-09-24 20:16:57 +0200

    introBBDD entrega de ejecicios

---
+++ introBBDD.sql
@@ -0,0 +1,18 @@
+SELECT flight_id, flight_no, status
+FROM flights
+WHERE status = 'On Time'
+
+SELECT *
+FROM bookings
+WHERE total_amount > 1000000
+
+SELECT *
+FROM Aircrafts
+
+SELECT flight_id
+FROM Flights
+WHERE aircraft_code = '733'
+
+SELECT *	
+FROM tickets
+WHERE passenger_name = 'VALERIY TIKHONOV'
